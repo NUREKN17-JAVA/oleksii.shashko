9583ecc64a06dbce416fa3c971078371838714d9
Oleksii Shashko <oleksii.shashko@nure.ua>
2019-10-15 10:04:51 +0300

    Steps 5-7: Implementate method Create

--- usermanagment/src/main/java/ua/nure/kn/shahsko/usermanagment/db/HsqldbUserDao.java
+++ usermanagment/src/main/java/ua/nure/kn/shahsko/usermanagment/db/HsqldbUserDao.java
@@ -2,10 +2,13 @@ package ua.nure.kn.shahsko.usermanagment.db;
 
 import ua.nure.kn.shahsko.usermanagment.domain.User;
 
+import java.sql.*;
 import java.util.Collection;
 
 public class HsqldbUserDao implements Dao<User> {
 
+    public static final String INSERT_QUERY = "INSERT INTO users (firstname, lastname, dateofbirth) VALUES (?, ?, ?)";
+    public static final String FUNCTION_IDETITY = "call IDENTITY()";
     private final ConnectionFactory connectionFactory;
 
     public HsqldbUserDao(ConnectionFactory connectionFactory) {
@@ -14,7 +17,36 @@ public class HsqldbUserDao implements Dao<User> {
 
     @Override
     public User create(User entity) throws DatabaseException {
-        return null;
+        try {
+            Connection connection = connectionFactory.createConnection();
+            PreparedStatement statement = connection
+                    .prepareStatement(INSERT_QUERY);
+            statement.setString(1, entity.getFirstName());
+            statement.setString(2, entity.getLastName());
+            statement.setDate(3, new Date(entity.getDateOfBirth().getTime()));
+            int numberOfRows = statement.executeUpdate();
+            if (numberOfRows != 1) {
+                throw new DatabaseException("Number of inserted rows: " + numberOfRows);
+            }
+
+            CallableStatement callableStatement = connection
+                    .prepareCall(FUNCTION_IDETITY);
+            ResultSet keys = callableStatement.executeQuery();
+            if (!keys.next()) {
+                entity.setId(keys.getLong(1)); // Mutation
+            }
+
+            keys.close();
+            callableStatement.close();
+            statement.close();
+            connection.close();
+
+            return entity;
+        } catch (DatabaseException e) {
+            throw e;
+        } catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
     }
 
     @Override
