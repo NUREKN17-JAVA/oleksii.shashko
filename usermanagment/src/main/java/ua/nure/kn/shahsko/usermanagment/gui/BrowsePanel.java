42d10e3ebe7e9b1fe62b5bd5cfd760ca0291fd76
Oleksii Shashko <oleksii.shashko@nure.ua>
2019-11-12 10:00:56 +0200

    Realization browse controls

---
+++ usermanagment/src/main/java/ua/nure/kn/shahsko/usermanagment/gui/BrowsePanel.java
@@ -0,0 +1,12 @@
+package ua.nure.kn.shahsko.usermanagment.gui;
+
+import javax.swing.*;
+
+public class BrowsePanel extends JPanel {
+
+    private final MainFrame parent;
+
+    public BrowsePanel(MainFrame mainFrame) {
+        parent = mainFrame;
+    }
+}
--- usermanagment/src/main/java/ua/nure/kn/shahsko/usermanagment/gui/MainFrame.java
+++ usermanagment/src/main/java/ua/nure/kn/shahsko/usermanagment/gui/MainFrame.java
@@ -1,7 +1,36 @@
 package ua.nure.kn.shahsko.usermanagment.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
 
+    private JPanel contentPanel;
+    private BrowsePanel browsePanel;
+
+    MainFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WIDTH, HEIGHT);
+        this.setTitle("Управление пользователями"); // localize
+        this.setContentPane(getContentPanel());
+    }
+
+    private JPanel getContentPanel() {
+        if(contentPanel == null) {
+            contentPanel = new JPanel();
+            contentPanel.setLayout(new BorderLayout());
+            contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+        }
+        return contentPanel;
+    }
+
+    private Component getBrowsePanel() {
+        if(browsePanel == null) {
+            browsePanel = new BrowsePanel(this);
+        }
+
+        return browsePanel;
+    }
 }
