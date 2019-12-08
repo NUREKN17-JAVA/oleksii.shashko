cebb17dfffc6abe19019df6a2bc4144fe151d1f5
Oleksii Shashko <oleksii.shashko@nure.ua>
2019-12-08 11:30:05 +0200

    Realizing support methods for agent searching

--- usermanagment/src/main/java/ua/nure/kn/shahsko/usermanagment/agent/SearchAgent.java
+++ usermanagment/src/main/java/ua/nure/kn/shahsko/usermanagment/agent/SearchAgent.java
@@ -1,6 +1,11 @@
 package ua.nure.kn.shahsko.usermanagment.agent;
 
 import jade.core.Agent;
+import ua.nure.kn.shahsko.usermanagment.db.DaoFactory;
+import ua.nure.kn.shahsko.usermanagment.db.DatabaseException;
+import ua.nure.kn.shahsko.usermanagment.domain.User;
+
+import java.util.Collection;
 
 public class SearchAgent extends Agent {
     protected void setup() {
@@ -11,5 +16,22 @@ public class SearchAgent extends Agent {
     protected void takeDown() {
         System.out.println(getAID().getName() + " terminated");
         super.takeDown();
+    }
+
+    public void search(String firstName, String lastName) throws SearchException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+            if (users.size() > 0) {
+                showUsers(users);
+            } else {
+                
+            }
+        } catch (DatabaseException | ReflectiveOperationException e) {
+            throw new SearchException(e);
+        }
+    }
+
+    private void showUsers(Collection<User> users) {
+
     }
 }
