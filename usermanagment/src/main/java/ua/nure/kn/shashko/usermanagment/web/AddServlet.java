8b242a7b174ff386df1650d621b1a854a5fb9cf9
Oleksii Shashko <oleksii.shashko@nure.ua>
2019-12-22 16:35:56 +0200

    Creating class of add servlet

--- usermanagment/src/main/java/ua/nure/kn/shashko/usermanagment/web/AddServlet.java
+++ usermanagment/src/main/java/ua/nure/kn/shashko/usermanagment/web/AddServlet.java
@@ -1,4 +1,24 @@
 package ua.nure.kn.shashko.usermanagment.web;
 
-public class AddServlet {
+import ua.nure.kn.shashko.usermanagment.db.DaoFactory;
+import ua.nure.kn.shashko.usermanagment.db.DatabaseException;
+import ua.nure.kn.shashko.usermanagment.domain.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddServlet extends EditServlet {
+    private static final String ADD_JSP = "/add,jsp";
+
+    @Override
+    protected void showPage(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        req.getRequestDispatcher(ADD_JSP).forward(req, resp);
+    }
+
+    @Override
+    protected void processUser(User user) throws ReflectiveOperationException, DatabaseException {
+        DaoFactory.getInstance().getUserDao().create(user);
+    }
 }
