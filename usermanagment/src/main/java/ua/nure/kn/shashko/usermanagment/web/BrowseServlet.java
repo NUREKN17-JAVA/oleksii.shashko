8fa75afb504e8940f4b335940a3e83f26005a497
Oleksii Shashko <oleksii.shashko@nure.ua>
2019-12-22 10:46:16 +0200

    Adding action performance for buttons in browseServlet

--- usermanagment/src/main/java/ua/nure/kn/shashko/usermanagment/web/BrowseServlet.java
+++ usermanagment/src/main/java/ua/nure/kn/shashko/usermanagment/web/BrowseServlet.java
@@ -12,14 +12,40 @@ import java.io.IOException;
 import java.util.Collection;
 
 public class BrowseServlet extends HttpServlet {
+    private static final String DETAILS_BUTTON = "detailsButton";
+    private static final String DELETE_BUTTON = "deleteButton";
+    private static final String DETAILS_JSP = "/details.jsp";
+    private static final String BROWSE_JSP = "/browse.jsp";
+    private static final String EDIT_BUTTON = "editButton";
+    private static final String ADD_BUTTON = "addButton";
+    private static final String EDIT_JSP = "/edit.jsp";
+    private static final String ADD_JSP = "/add.jsp";
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getParameter("addButton") != null) {
+        if (req.getParameter(ADD_BUTTON) != null) {
             add(req, resp);
-        } // else if other commands
-        else {
+        } else if (req.getParameter(EDIT_BUTTON) != null) {
+            edit(req, resp);
+        } else if (req.getParameter(DELETE_BUTTON) != null) {
+            delete(req, resp);
+        } else if (req.getParameter(DETAILS_BUTTON) != null) {
+            details(req, resp);
+        } else {
             browse(req, resp);
         }
+    }
+
+    private void details(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    private void edit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 
     private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
